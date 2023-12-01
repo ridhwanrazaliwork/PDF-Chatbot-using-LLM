7f389bc4fdc47a6d449f076b00436510802b5d43
safwanshamsir99 <safwanshamsir7177@gmail.com>
2023-12-01 16:12:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 import streamlit as st
 from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+import openai
+
+# Access the OpenAI API key
+openai.api_key = st.secrets["api_key"]
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -8,6 +15,21 @@ def get_pdf_text(pdf_docs):
         for page in pdf_reader.pages:
             text += page.extract_text()
     return text
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings(openai_api_key=openai.api_key)
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
 
 def main():
     st.set_page_config(page_title="Chat with multiple PDFs",
@@ -24,11 +46,12 @@ def main():
             with st.spinner("Processing"):
                 # get pdf text
                 raw_text = get_pdf_text(pdf_docs)
-                st.write(raw_text)
 
                 # get the text chunks
+                text_chunks = get_text_chunks(raw_text)
 
                 # create vector store
+                vectorstore = get_vectorstore(text_chunks)
 
 
 if __name__ == '__main__':
